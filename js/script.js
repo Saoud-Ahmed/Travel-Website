c471fa07e20490e260e999615325845c7eb9e4ba
Saoud Ahmed <135643500+Saoud-Ahmed@users.noreply.github.com>
2024-09-08 20:56:27 +0500

    s

--- js/script.js
+++ js/script.js
@@ -257,5 +257,8 @@ function filterTrips() {
         }
     });
 }
-
+(function(w,d,s,r,n){w.TrustpilotObject=n;w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)};
+            a=d.createElement(s);a.async=1;a.src=r;a.type='text/java'+s;f=d.getElementsByTagName(s)[0];
+            f.parentNode.insertBefore(a,f)})(window,document,'script', 'https://invitejs.trustpilot.com/tp.min.js', 'tp');
+            tp('register', '7QKKHair54lTU5tI');
 
